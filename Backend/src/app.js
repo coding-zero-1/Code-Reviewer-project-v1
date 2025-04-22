f55489f50070acbb6a79ca36d058b582c41452af
coding-zero-I <yashsangwan4700@gmail.com>
2025-04-22 09:18:06 +0530

    added basic rate limiting

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,15 +3,18 @@ const app = express()
 const aiRoutes = require("./routes/ai.routes")
 const cors = require("cors")
 const userRouter = require("./routes/user")
+const rateLimiter = require("express-rate-limit")
+const limiter = rateLimiter({
+    max:5,
+    windowMs:60*1000,
+    message:'too many requests, please wait for a minute',
+    standardHeaders:true,
+    legacyHeaders:false
+});
 
+app.use(limiter);
 app.use(express.json())
 app.use(cors())
-
-app.get("/",(req,res)=>{
-    res.json({
-        msg:"Hello there"
-    })
-})
 
 app.use('/user',userRouter)
 app.use("/ai",aiRoutes)
